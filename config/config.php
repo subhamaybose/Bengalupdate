b185d0ccf335331f42b6d3f70491ab347e3d73d9
subhamaybose <subhamaybose@gmail.com>
2019-09-18 21:04:45 +0530

    Version Updated

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 //ini_set('session.save_path','/home/bengalup/' . '/tmp');
 #$conn=mysql_connect('localhost','BengalSom','SukMitBen@2016#%') or die('Error: connection');
 #$db=mysql_select_db('bengalup_newdb',$conn);
-$conn_archive=@mysql_connect('localhost','root','nopass@12345',true) or die('Error: connection');
-$conn=@mysql_connect('localhost','root','nopass@12345',true) or die('Error: connection');
-$db_archive=mysql_select_db('newsdb_archive',$conn_archive);
-$db=mysql_select_db('newsdb',$conn);
+$conn_archive=@mysql_connect('localhost','XXXXXXXXXX','XXXXXXXXXX',true) or die('Error: connection');
+$conn=@mysql_connect('localhost','XXXXXXXXXX','XXXXXXXXXX',true) or die('Error: connection');
+$db_archive=mysql_select_db('XXXXXXXXXX',$conn_archive);
+$db=mysql_select_db('XXXXXXXXXX',$conn);
 $siteurl="";
 $adminsiteurl="";
 #$siteurl="http://bengalupdate.com/bengalupdate-new/";
